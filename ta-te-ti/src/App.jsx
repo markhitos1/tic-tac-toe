c47ab9cf83572cbdf707efa649332ff73ac1bf33
markhitos <mc2622016@gmail.com>
2024-11-17 23:24:56 -0300

    ajutse de juegos

--- ta-te-ti/src/App.jsx
+++ ta-te-ti/src/App.jsx
@@ -14,7 +14,7 @@ const Square = ({ children,isSelected, updateBoard, index })=>{
   const className =`square ${isSelected ? 'is-selected' : ''}`
  
  const handleClick = () => {
-   updateBoard()
+   updateBoard(index)
  }
   return (
     <div  onClick={handleClick} className={className} >
@@ -23,16 +23,50 @@ const Square = ({ children,isSelected, updateBoard, index })=>{
   )
 } 
 
+
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
+
 function App() {
   const [board, setBoard] = useState(
     Array(9).fill(null)
   )
   const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null)
 
-  const updateBoard = () => {
+  const checkWinner = () => {
+    for(const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if(boardCheck[a] &&
+        boardCheck[a] === boardCheck[b] &&
+        boardCheck[a] === boardCheck[c]
+        ) {
+          return boardCheck[a]
+  }
+  return null
+}
+  const updateBoard = (index) => {
+    if (board[index] || winner ) return
+   const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
     const newTurn = turn ===  TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    const newWinner = checkWinner(newBoard)
+    if(newWinner) {
+      setWinner(newWinner)
+    }
   }
+
   
   return (
     <main className="board">
